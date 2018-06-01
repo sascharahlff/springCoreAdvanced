2bbc8c417af2ab1f948499cc01706b93d0916ef9
Sascha Rahlff <srRahlff@itemis.com>
2018-06-01 08:44:03 +0200

    Remove Transaction Management from Spring Boot. Configure to run in Spring.

--- src/main/java/guru/springframework/config/H2DataSourceConfig.java
+++ src/main/java/guru/springframework/config/H2DataSourceConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 /**
  * Created by jt on 1/7/16.
  */
 @Configuration
+//@EnableTransactionManagement => Bug
 public class H2DataSourceConfig {
-
     @Bean
     public DataSource dataSource(){
         DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
